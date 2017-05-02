5f8ae5413a2df879380415f74f418d5c0fb98f37
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-05-02 22:07:05 +0900

    Add comparison
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- small_step_semantics.rb
+++ small_step_semantics.rb
@@ -75,6 +75,92 @@ class Machine < Struct.new(:expression)
   end
 end
 
+class Boolean < Stuct.new(:value)
+  def to_s
+    value.to_s
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reducible?
+    false
+  end
+end
+
+class LessThan < Struct.new(:left, :right)
+  def to_s
+    "#{left} < #{right}"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce
+    if left.reducible?
+      LessThan.new(left.reduce, right)
+    elsif right.reducible?
+      LessThan.new(left, right.reduce)
+    else
+      Boolean.new(left.value < right.value)
+    end
+  end
+end
+
+class GreaterThan < Struct.new(:left, :right)
+  def to_s
+    "#{left} > #{right}"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce
+    if left.reducible?
+      GreaterThan.new(left.reduce, right)
+    elsif right.reducible?
+      GreaterThan.new(left, right.reduce)
+    else
+      Boolean.new(left.value > right.value)
+    end
+  end
+end
+
+class EqualTo < Struct.new(:left, :right)
+  def to_s
+    "#{left} == #{right}"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce
+    if left.reducible?
+      EqualTo.new(left.reduce, right)
+    elsif right.reducible?
+      EqualTo.new(left, right.reduce)
+    else
+      Boolean.new(left.value == right.value)
+    end
+  end
+end
+
 Machine.new(
   Add.new(
     Multiply.new(Number.new(1), Number.new(2)),
