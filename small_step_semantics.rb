918be519d1780a010336e96b89ecef2cd38736d1
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-05-02 21:05:03 +0900

    Add initial definition of Add, Multiply, Number
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

---
+++ small_step_semantics.rb
@@ -0,0 +1,34 @@
+class Number < Struct.new(:value)
+  def to_s
+    value.to_s
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
+class Add < Struct.new(:left, :right)
+  def to_s
+    "#{left} + #{right}"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
+class Multiply < Struct.new(:left, :right)
+  def to_s
+    "#{left} * #{right}"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
+puts Add.new(
+  Multiply.new(Number.new(1), Number.new(2)),
+  Multiply.new(Number.new(3), Number.new(5))
+)
