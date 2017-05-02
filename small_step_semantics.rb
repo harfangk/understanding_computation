9ded7fafb08b3e8d6deb8e4e999da1665fba0b5b
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-05-02 21:59:56 +0900

    Add reduce
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- small_step_semantics.rb
+++ small_step_semantics.rb
@@ -24,6 +24,16 @@ class Add < Struct.new(:left, :right)
   def reducible?
     true
   end
+
+  def reduce
+    if left.reducible?
+      Add.new(left.reduce, right)
+    elsif right.reducible?
+      Add.new(left, right.reduce)
+    else
+      Number.new(left.value + right.value)
+    end
+  end
 end
 
 class Multiply < Struct.new(:left, :right)
@@ -37,6 +47,16 @@ class Multiply < Struct.new(:left, :right)
 
   def reducible?
     true
+  end
+
+  def reduce
+    if left.reducible?
+      Multiply.new(left.reduce, right)
+    elsif right.reducible?
+      Multiply.new(left, right.reduce)
+    else
+      Number.new(left.value * right.value)
+    end
   end
 end
 
