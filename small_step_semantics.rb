8c9ca77ec790a7feff79b92546477bc6141f4cb7
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-05-02 22:02:05 +0900

    Add Machine
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- small_step_semantics.rb
+++ small_step_semantics.rb
@@ -60,7 +60,24 @@ class Multiply < Struct.new(:left, :right)
   end
 end
 
-puts Add.new(
-  Multiply.new(Number.new(1), Number.new(2)),
-  Multiply.new(Number.new(3), Number.new(5))
-)
+class Machine < Struct.new(:expression)
+  def step
+    self.expression = expression.reduce
+  end
+
+  def run
+    while expression.reducible?
+      puts expression
+      step
+    end
+
+    puts expression
+  end
+end
+
+Machine.new(
+  Add.new(
+    Multiply.new(Number.new(1), Number.new(2)),
+    Multiply.new(Number.new(3), Number.new(5))
+  )
+).run
